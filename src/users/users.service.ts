cce42e4f6a61afb4625d14c26fe9dec042292ea4
Damjan Piperski <damjanpiperski@gmail.com>
2021-06-06 00:29:14 +0200

    app

---
+++ src/users/users.service.ts
@@ -0,0 +1,15 @@
+/**
+ * Data Model Interfaces
+ */
+
+import { BaseUser, User } from "./user.interface";
+import { Users } from "./users.interface";
+
+/**
+ * In-Memory Store
+ */
+
+
+/**
+ * Service Methods
+ */
